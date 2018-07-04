f10196b6af0c7b163a4480197c98f116b9aac859
Ice Lam <pinky.yy.lam@gmail.com>
2018-07-04 04:25:14 +0000

    Init commit for "Show me the IP"

---
+++ Show me the IP/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Show me the IP
+//
+//  Created by Pinky Lam on 8/5/2018.
+//  Copyright © 2018 Pinky Lam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
+
